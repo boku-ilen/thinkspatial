d098d54fbf3c68b69a95f3e3cd4154a736d59e58
patrickcipot <patrickcipot@141.244.85.51>
2018-09-26 12:59:22 +0200

    * GeoJSON performance improved

---
+++ thinkspatial_web/custom_sqls.py
@@ -0,0 +1,15 @@
+from django.db import connection
+
+def get_attributes(layer):
+    with connection.cursor() as cursor:
+        cursor.execute("""select a.name, case when a."type"=1 then av.string_value::varchar when a."type"=2 then av.integer_value::varchar when a."type"=3 then av.float_value::varchar when a."type"=4 then av.date_value::varchar end  as value from thinkspatial_web_attribute as a
+join thinkspatial_web_view as v on a.id = v.attribute_id
+join thinkspatial_web_attributevalue as av on a.id = av.attribute_id
+where v.enabled = true and v.layer_id = 8 order by av.id""", [layer])
+        rs = cursor.fetchall()
+        
+        cursor.execute("select count(id) from thinkspatial_web_view where layer_id = %s", [layer])
+        count = cursor.fetchone()
+        
+        
+    return [rs, count[0]]
